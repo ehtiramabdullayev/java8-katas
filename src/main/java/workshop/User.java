b1a0599e433163b7235e738a46bf43c1678358d7
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2022-07-13 00:11:07 +0200

    added new workshop related exercises

--- src/main/java/workshop/User.java
+++ src/main/java/workshop/User.java
@@ -3,11 +3,32 @@ package workshop;
 public class User {
     private final int id;
     private final String name;
-    private final String surName;
+    private final String surname;
 
-    public User(int id, String name, String surName) {
+    public User(int id, String name, String surname) {
         this.id = id;
         this.name = name;
-        this.surName = surName;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
     }
 }
